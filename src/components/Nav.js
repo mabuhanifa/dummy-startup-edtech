8a570d16308ca0e8866f2ef29962001345f91d4b
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-01 01:35:46 +0600

    className="dark"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,12 +7,12 @@ export default function Nav() {
     <nav className='flex justify-between 2xl:mx-40 my-10'>
         <div>Logo</div>
         <div className='flex gap-5'>
-            <a href="">Home</a>
-            <a href="">Course</a>
-            <a href="">Price</a>
-            <a href="">About</a>
+            <a href="/">Home</a>
+            <a href="/">Course</a>
+            <a href="/">Price</a>
+            <a href="/">About</a>
         </div>
-        <div>
+        <div className='flex gap-x-5'>
             <button className='px-5 py-2 bg-blue-700 text-white font-bold rounded-full'>SIGN UP</button>
             <div
             className="flex justify-center items-center cursor-pointer "
