b7cbb47c19b0ca09ebe75e44430554cff69ad210
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-02 23:11:47 +0600

    nav changed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { BsSun } from "react-icons/bs";
-import { FaMoon } from "react-icons/fa";
+import React from "react";
 
 export default function Nav() {
-  const [theme, setTheme] = useState("light");
-
-  // if local storage is empty save theme as light
-  useEffect(() => {
-    if (localStorage.getItem("theme") === null) {
-      localStorage.setItem("theme", "light");
-    }
-  }, []);
-
-  useEffect(() => {
-    // select html elem
-    const html = document.querySelector("html");
-    if (localStorage.getItem("theme") === "dark") {
-      html.classList.add("dark");
-      setTheme("dark");
-    } else {
-      html.classList.remove("dark");
-      setTheme("light");
-    }
-  }, [theme]);
-
-  // handle switch theme
-  const handleTheme = () => {
-    if (localStorage.getItem("theme") === "light") {
-      setTheme("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      setTheme("light");
-      localStorage.setItem("theme", "light");
-    }
-  };
 
   return (
     <nav className="flex justify-between mb-20">
@@ -43,22 +10,11 @@ export default function Nav() {
         <a href="/">Course</a>
         <a href="/">Price</a>
         <a href="/">About</a>
-        <div
-          className="flex justify-center items-center cursor-pointer cursor:pointer"
-          onClick={handleTheme}
-        >
-          {theme === "dark" ? (
-            <BsSun className="inline text-xl" />
-          ) : (
-            <FaMoon className="inline text-xl" />
-          )}
-        </div>
-      </div>
-      <div className="flex gap-x-5">
         <button className="px-5 py-2 bg-blue-700 text-white font-bold rounded-full">
           SIGN UP
         </button>
       </div>
+
     </nav>
   );
 }
