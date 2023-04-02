69695dc39b65c06bef21008a30a966fc86736ed0
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-02 23:22:10 +0600

    logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
 import React from "react";
-
+import logo from '../images/logo.jpeg';
 export default function Nav() {
 
   return (
     <nav className="flex justify-between mb-20">
-      <div className="flex gap-x-10 justify-center items-center">Logo</div>
+      <div className="flex gap-x-10 justify-center items-center">
+        <img src={logo} alt="" className="w-20 rounded"/>
+      </div>
       <div className="flex gap-x-10 justify-center items-center">
         <a href="/">Home</a>
-        <a href="/">Course</a>
-        <a href="/">Price</a>
+        <a href="/">Courses</a>
+        <a href="/">Bank Job</a>
+        <a href="/">BCS Preparation</a>
         <a href="/">About</a>
         <button className="px-5 py-2 bg-blue-700 text-white font-bold rounded-full">
           SIGN UP
