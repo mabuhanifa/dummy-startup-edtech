0d955a8167f360dce67ec816d5fa60d80c18d3a6
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-03 01:27:36 +0600

    slider working

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import logo from "../images/logo.jpeg";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Slider() {
   return (
-    <div>Slider</div>
-  )
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={100}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      className="p-10"
+    >
+      <SwiperSlide >
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={logo} alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Nav from '../components/Nav'
+import Slider from '../components/Slider'
 
 export default function Home() {
   return (
     <div>
         <Nav/>
         <Hero/>
+        <div className='my-10'>
+        <Slider />
+        </div>
     </div>
   )
 }
