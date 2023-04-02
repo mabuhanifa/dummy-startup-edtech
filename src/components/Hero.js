8519c34544430fc8c39872aee832e8bb4639d985
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-03 00:04:54 +0600

    background-image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="back-ground">
+      <div className="px-20">
+        <div>
+          <h1 className="text-[75px] font-bold">
+            পড়াশুনা <br /> এখন <br />
+            আরো সহজ
+          </h1>
+        </div>
+        <div>
+          <button className="px-10 py-3 border border-gray-400 text-indigo-500 rounded-md mt-10 hover:bg-indigo-700 hover:text-white uppercase font-bold mb-40">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
