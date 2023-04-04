ef87442156d28fb60d2ff9fd865da5ce95fbcf1b
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-05 01:21:17 +0600

    title and padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="2xl:px-40 py-10 dark:bg-gray-700 dark:text-gray-300">
+    <div className="2xl:px-40 xl:px-20 px-10 py-10 dark:bg-gray-700 dark:text-gray-300">
       <Home />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer>
-      <h1 class="f-t">Goal</h1>
+      <h1 class="f-t">Victors</h1>
       <hr class="hr" />
       <div class="footer-items">
         <a href="/">Contact Us</a>
         <a href="/">Terms of services</a>
         <a href="/">Privacy Policy</a>
         <a href="/">Privacy Settings</a>
-        <a href="/">Goal App</a>
-        <a href="/">Goal Live</a>
+        <a href="/">Mobile App</a>
+        <a href="/">About</a>
       </div>
       <div class="social-media">
         <a
