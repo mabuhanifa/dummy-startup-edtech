60c83207a7e0a81523d0c84f2db1569554d4a94f
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-06 09:54:36 +0600

    student image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import student from "../images/student.jpg";
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <div>
+        <div>
+          <img src={student} alt="" />
+        </div>
+        <div>
+          <span>WHO WE ARE</span>
+          <h1>শিক্ষা হোক সময়োপযোগী</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
