757a6b00d2f3919953f8258610c1bc78c317930d
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-06 10:28:14 +0600

    bank

--- src/pages/Bank.js
+++ src/pages/Bank.js
@@ -1,7 +1,38 @@
 import React from 'react'
-
+import bank from '../images/bank.jpg'
 export default function Bank() {
   return (
-    <div>Bank</div>
+    <div>
+      <div className="md:flex justify-center">
+        <div className="w-full">
+          <img src={bank} alt="" className="" />
+        </div>
+        <div className="w-full mt-10">
+          <h1 className="text-4xl font-bold my-10">ব্যাংক জব প্রস্তুতি</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            tenetur incidunt officia esse explicabo tempora soluta quis. Cum
+            delectus ut magnam, totam corrupti quia dolorum amet, autem a
+            laudantium rem quidem aspernatur? Odio, incidunt perferendis
+            accusantium quisquam provident quibusdam enim deserunt perspiciatis
+            totam exercitationem eum sequi in nisi ab numquam!
+          </p>
+          <div className="md:flex mt-5">
+            <div className=" my-10">
+              <h2 className="text-xl font-bold my-5">Total Videos</h2>
+              <p className="text-6xl font-black text-indigo-700">55</p>
+            </div>
+            <div className="md:mx-20 my-10">
+              <h2 className="text-xl font-bold my-5">Total Hours Of Video</h2>
+              <p className="text-6xl font-black text-indigo-700">40</p>
+            </div>
+            <div className=" my-10">
+              <h2 className="text-xl font-bold my-5">Total PDF</h2>
+              <p className="text-6xl font-black text-indigo-700">21</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
