630727590f8016caa8a5a68742727752d3d208d0
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-06 10:12:30 +0600

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,35 @@ import student from "../images/student.jpg";
 export default function About() {
   return (
     <div>
-      <div>
-        <div>
-          <img src={student} alt="" />
+      <div className="md:flex justify-center">
+        <div className="w-full">
+          <img src={student} alt="" className="" />
         </div>
-        <div>
-          <span>WHO WE ARE</span>
-          <h1>শিক্ষা হোক সময়োপযোগী</h1>
+        <div className="w-full md:mt-40">
+          <span>____ WHO WE ARE</span>
+          <h1 className="text-4xl font-bold my-10">শিক্ষা হোক সময়োপযোগী</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            tenetur incidunt officia esse explicabo tempora soluta quis. Cum
+            delectus ut magnam, totam corrupti quia dolorum amet, autem a
+            laudantium rem quidem aspernatur? Odio, incidunt perferendis
+            accusantium quisquam provident quibusdam enim deserunt perspiciatis
+            totam exercitationem eum sequi in nisi ab numquam!
+          </p>
+          <div className="md:flex mt-5">
+            <div className=" my-10">
+              <h2 className="text-xl font-bold my-5">Total Courses</h2>
+              <p className="text-6xl font-black text-indigo-700">5</p>
+            </div>
+            <div className="md:mx-10 my-10">
+              <h2 className="text-xl font-bold my-5">Total Hours Of Video</h2>
+              <p className="text-6xl font-black text-indigo-700">240</p>
+            </div>
+            <div className=" my-10">
+              <h2 className="text-xl font-bold my-5">Total Instructor</h2>
+              <p className="text-6xl font-black text-indigo-700">7</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
